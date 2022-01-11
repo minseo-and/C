7d6ec961b437ddd06e47a0e4a5ebc259c3d9d8e9
minseo <msp05070478@gmail.com>
2022-01-12 06:55:42 +0900

    1004

---
+++ 기초100제/cmake-build-debug/CMakeFiles/3.21.1/CMakeFiles/100.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "100"
+  "100.pdb"
+  "CMakeFiles/100.dir/1001.c.o"
+  "CMakeFiles/100.dir/1002.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/100.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
